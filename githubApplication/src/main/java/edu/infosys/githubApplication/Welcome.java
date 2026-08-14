3d241a5a100b9322b2ea6e77c0faeebcceb09026
Visnu15 <psvisnu15@gmail.com>
2026-08-14 15:39:18 +0530

    Second Version

---
+++ githubApplication/src/main/java/edu/infosys/githubApplication/Welcome.java
@@ -0,0 +1,7 @@
+package edu.infosys.githubApplication;
+
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Welcome to GitHub Application");
+    }
+}
